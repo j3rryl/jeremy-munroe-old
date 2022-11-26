382e6b98d71e671f92d129dcf178971276af41bc
j3rryl <jeremymunroe0@gmail.com>
2022-11-26 10:52:33 +0300

    Splash screen

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -9,7 +9,7 @@ const Splash = () => {
   
   return (
     <motion.div 
-    className='motion'
+    className='w-full h-full m-auto text-center'
     // initial={{scaleY:0}} 
     // animate={{scaleY:1}}
     // exit={{scaleY:0}}
@@ -18,19 +18,18 @@ const Splash = () => {
     animate={{opacity:1}}
     exit={{opacity:0}}
     >
-    <div className='splash-screen'>
-      <div className='canvas'>
+    <div className='m-0 w-full h-full text-center overflow-hidden'>
+      <div className='w-full h-full absolute'>
       <CanvasScene children={<Stars />} />
       </div>
-      <div className='details'>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      <h1 className='title'>Jeremy Munroe</h1>
-      <h1 className='title'>UI/UX Developer</h1>
-      <button className='explore' onClick={()=>{navigate('/home')}}>
-        Explore
-      </button>
+      <div className='w-full h-full m-auto text-center flex justify-center items-center'>
+        <div className='w-fit inline-block justify-around items-center'>
+          <h1 className='my-4 tracking-[.35em] text-center text-xl'>Jeremy Munroe</h1>
+          <h1 className='my-4 tracking-[.35em] text-center text-xl'>UI/UX Developer</h1>
+          <button className='explore px-10 py-5 text-base my-8 mx-auto text-center h-fit w-fit relative' onClick={()=>{navigate('/home')}}>
+          Explore
+          </button>
+        </div>
       </div>
       
     </div>
