d8cc767d1cc2af7cc3971ff4f032238824e30c52
j3rryl <jeremymunroe0@gmail.com>
2022-12-01 14:04:34 +0300

    backdrop filter issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,28 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import '../assets/css/navbar.css';
 
 function Navbar() {
 	const navRef = useRef();
-  const [mobile, setMobile]=useState(false)
-  useEffect({
-    
-  }, [mobile])
 	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
+		navRef.current.classList.toggle("responsive_navbar");
 	};
 
 	return (
-		<header>
-			<h3 className=' font-caveat text-2xl'>Jeremy Munroe</h3>
-			<nav ref={navRef}>
+		<header className='h-20 w-full px-8 z-10 bg-white flex items-center justify-between'>
+			<h3 className='font-caveat text-2xl'>Jeremy Munroe</h3>
+			<nav ref={navRef} className='flex items-center text-2xl sm:text-xl font-caveat'>
 				<Link to='/home'>Home</Link>
 				<Link to='/about'>About</Link>
-        <Link to='/projects'>Case Studies</Link>
+        		<Link to='/projects'>Case Studies</Link>
 				<button
-					className="nav-btn nav-close-btn"
+					className="nav-btn sm:hidden visible p-1 text-3xl absolute top-10 right-10"
 					onClick={showNavbar}>
 					<FaTimes />
 				</button>
 			</nav>
-			<button className="nav-btn" onClick={showNavbar}>
+			<button className='nav-btn p-1 text-3xl sm:hidden visible opacity-100' onClick={showNavbar}>
 				<FaBars />
 			</button>
 		</header>
--- src/routes/RoutesHandler.js
+++ src/routes/RoutesHandler.js
@@ -1,4 +1,5 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import { AnimatePresence } from 'framer-motion'
 import Splash from '../pages/Splash'
 import Home from '../pages/Home'
@@ -6,16 +7,21 @@ import About from '../pages/About'
 import Navbar from '../components/Navbar'
 
 const RoutesHandler = () => {
+    const [inIndex, setInIndex] = useState();
     const location=useLocation()
-    console.log(location)
+    useEffect(() => {
+      //Checks if location.pathname is not "/".
+      setInIndex(location.pathname === "/")
+    }, [location.pathname]);
+
   return (
     <AnimatePresence mode='wait'>
+      {!inIndex?<Navbar />:null}
     <Routes location={location} key={location.pathname}>
       <Route path='/' element={<Splash />}/>
       <Route path='/portfolio' element={<Splash />}/>
       <Route path='/home' element={<Home />}/>
       <Route path='/about' element={<About />}/>
-      <Route path='/nav' element={<Navbar />}/>
       {/* <Route path='/projects' element={<Projects />}/>
       <Route path='/cases' element={<Cases />}/>
       <Route path='/contact' element={<Contact />}/> */}
