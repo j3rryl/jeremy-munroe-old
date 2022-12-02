9c0b598fdeaf812c81c6207dc6240948030d749d
j3rryl <jeremymunroe0@gmail.com>
2022-12-02 03:14:08 +0300

    hover effects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import '../assets/css/navbar.css';
 
 function Navbar() {
@@ -20,9 +20,9 @@ function Navbar() {
 		<header className={`${inIndex?'hidden':'visible'} h-20 w-full px-8 z-10 bg-white flex items-center justify-between`}>
 			<h3 className='font-caveat text-2xl'>Jeremy Munroe</h3>
 			<nav ref={navRef} className='flex items-center text-2xl sm:text-xl font-caveat'>
-				<Link to='/jeremy-munroe/home'>Home</Link>
-				<Link to='/jeremy-munroe/about'>About</Link>
-        		<Link to='/jeremy-munroe/projects'>Case Studies</Link>
+				<NavLink to='/jeremy-munroe/home'>Home</NavLink>
+				<NavLink to='/jeremy-munroe/about'>About</NavLink>
+        		<NavLink to='/jeremy-munroe/projects'>Case Studies</NavLink>
 				<button
 					className="nav-btn sm:hidden visible p-1 text-3xl absolute top-10 right-10"
 					onClick={showNavbar}>
