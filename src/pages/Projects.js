963d923f5123cc8dee966c163b4c18cb77caa9d3
j3rryl <jeremymunroe0@gmail.com>
2022-12-02 03:31:20 +0300

    ...

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,30 @@
+import '../assets/scss/home.scss'
+import { motion } from 'framer-motion'
+import { lazy } from 'react'
+import CanvasScene from '../components/CanvasScene'
 const Projects = () => {
+  const StarScene = lazy(() => import('../components/Stars'))
   return (
-    <div>Projects</div>
+    <motion.div 
+    className='motion'
+    // initial={{scaleY:0}} 
+    // animate={{scaleY:1}}
+    // exit={{scaleY:0}}
+    // transition={{duration : 1}}
+    // initial={{opacity:0}} 
+    // animate={{opacity:1}}
+    // exit={{opacity:0}}
+    >
+    <div className='projects-page md:overflow-x-hidden w-full h-full bg-none'>
+      {/* Cover Page */}
+      <div className='canvas w-full h-full fixed'>
+      <CanvasScene children={<StarScene/>}/>
+      </div>
+      <div>
+        Projects
+      </div>
+    </div>
+    </motion.div>
   )
 }
 
